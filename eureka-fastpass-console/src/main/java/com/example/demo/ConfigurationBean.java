8e3a3d4a6d5918e2624e499d9cb7dcab1cd13d75
Assaduzzaman Assad <a2z.ice@gmail.com>
2020-06-23 05:54:05 +0600

    added webclient to support eureka

--- eureka-fastpass-console/src/main/java/com/example/demo/ConfigurationBean.java
+++ eureka-fastpass-console/src/main/java/com/example/demo/ConfigurationBean.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @PropertySource("classpath:common.properties")
 @Configuration
@@ -27,6 +30,13 @@ public class ConfigurationBean {
 	public String getFastPassServiceLocation() {
 		return fastPassServiceLocation;
 	}
+	
+	 @Bean
+	    WebClient webClient(LoadBalancerClient loadBalancerClient) {
+	        return WebClient.builder()
+	                .filter(new LoadBalancerExchangeFilterFunction(loadBalancerClient))
+	                .build();
+	    }	
 	
 		
 }
--- eureka-fastpass-console/src/main/java/com/example/demo/controller/FastPassController.java
+++ eureka-fastpass-console/src/main/java/com/example/demo/controller/FastPassController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.demo.ConfigurationBean;
 import com.example.demo.model.FastPassCustomer;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.HttpHeaders;
 
 //@RibbonClient(name="pluralsight-fastpass-service", configuration=TollClientRoutingConfig.class)
 @Controller
@@ -20,9 +25,10 @@ public class FastPassController {
 	@Autowired
 	private ConfigurationBean config;
 	
-	
-	public FastPassController(RestTemplate restTemplate) {
+	private final WebClient webClientInstance;
+	public FastPassController(RestTemplate restTemplate,WebClient webClientInstance) {
 		this.restTemplate = restTemplate;
+		this.webClientInstance = webClientInstance;
 	}
 	
 //	@HystrixCommand(fallbackMethod="getFastPassCustomerDetailsBackup")
@@ -34,6 +40,20 @@ public class FastPassController {
 		m.addAttribute("customer", c);
 		
 		return "console";
+	}
+	@RequestMapping(path="/webclient", params={"fastpassid"})
+	public String getServiceUsingWebClient(@RequestParam String fastpassid, Model m) {
+		FastPassCustomer c = webClientInstance
+                .get()
+                .uri(config.getFastPassServiceLocation() + "?fastpassid=" +fastpassid)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToMono(FastPassCustomer.class)
+                .block();
+		m.addAttribute("customer", c);
+		return "console";
+		
 	}
 	
 	public String getFastPassCustomerDetailsBackup(@RequestParam String fastpassid, Model m) {
