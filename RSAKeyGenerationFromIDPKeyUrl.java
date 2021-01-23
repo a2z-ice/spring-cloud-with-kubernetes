06b31597c76f8b359d63f4d8584cf26805d3b7a7
Assaduzzaman <a2z.ice@gmail.com>
2021-01-23 22:23:14 +0600

    Update and rename TokenValidationTests.java to RSAKeyGenerationFromIDPKeyUrl.java

--- RSAKeyGenerationFromIDPKeyUrl.java
+++ RSAKeyGenerationFromIDPKeyUrl.java
@@ -8,7 +8,7 @@ import java.security.PublicKey;
 import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
 
-public class TokenValidationTests {
+public class RSAKeyGenerationFromIDPKeyUrl {
 	
 	public static void main(String [] args) throws IOException {
 		String modulusStr = "ipjRdnJXyCZopR1jFvV8RRnjbn834DLlS577i-5c_PWk_qGNSMqT7wL7ibN442-7HAcGdx5ZG9bsAmfKM3Fx987CZCeh2Kpih1JECTL-iyB_T9pditDQgYF1Y0dHnR0OjeUelVwi-YLsgEI8_8INoNrHOxXDD3o2eWMDyHVvOn7g2MnYT-v24-0k_Mdll32tW4z3dIKJdj0w9FG4zvnD3C6hJpsUArXWGvQMeF_BEE5ccPqJggsd1GAkjhypRjLi2g3h2kOhr4JZS3NUb3ybQJB00w2BxAbpEty9A2ERgEoL-zepWveRxsSEVs0zF5mtFGa91LGig74gRxAnLEgQww";
