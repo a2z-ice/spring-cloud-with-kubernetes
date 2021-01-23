016ec9ad736f1e8f0eeaf2ce25ef802925dc0caf
Assaduzzaman <a2z.ice@gmail.com>
2021-01-23 22:24:00 +0600

    Update RSAKeyGenerationFromIDPKeyUrl.java

--- RSAKeyGenerationFromIDPKeyUrl.java
+++ RSAKeyGenerationFromIDPKeyUrl.java
@@ -11,8 +11,8 @@ import java.util.Base64;
 public class RSAKeyGenerationFromIDPKeyUrl {
 	
 	public static void main(String [] args) throws IOException {
-		String modulusStr = "ipjRdnJXyCZopR1jFvV8RRnjbn834DLlS577i-5c_PWk_qGNSMqT7wL7ibN442-7HAcGdx5ZG9bsAmfKM3Fx987CZCeh2Kpih1JECTL-iyB_T9pditDQgYF1Y0dHnR0OjeUelVwi-YLsgEI8_8INoNrHOxXDD3o2eWMDyHVvOn7g2MnYT-v24-0k_Mdll32tW4z3dIKJdj0w9FG4zvnD3C6hJpsUArXWGvQMeF_BEE5ccPqJggsd1GAkjhypRjLi2g3h2kOhr4JZS3NUb3ybQJB00w2BxAbpEty9A2ERgEoL-zepWveRxsSEVs0zF5mtFGa91LGig74gRxAnLEgQww";
-		String exponentStr = "AQAB";
+		String modulusStr = "value_of_n";
+		String exponentStr = "value_of_e";
 		
 		BigInteger modulus = new BigInteger(1, base64UrlDecode(modulusStr));
 		BigInteger publicExponent = new BigInteger(1, base64UrlDecode(exponentStr));
