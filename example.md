2e3c6a56b4f6e6959b0126d0df3646aeebf67d43
Assaduzzaman <a2z.ice@gmail.com>
2021-08-05 20:43:00 +0600

    Add files via upload

---
+++ example.md
@@ -0,0 +1,3 @@
+```
+systemctl is-active --quiet example.service || systemctl restart example.service
+```
---
+++ example.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+INDEX=0; while true; do echo "Press CTRL+C to stop the script execution $INDEX"; sleep 2;  INDEX=$(($INDEX+1)); done
