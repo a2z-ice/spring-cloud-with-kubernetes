f2d51408075b5fabe59ac3eee91bf815572694d7
Assaduzzaman <a2z.ice@gmail.com>
2023-02-02 05:47:43 +0600

    added hash generation and validation

--- cache/l2cache/src/main/java/com/example/l2cache/DynamicSqlRepo.java
+++ cache/l2cache/src/main/java/com/example/l2cache/DynamicSqlRepo.java
@@ -4,27 +4,51 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 
+import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.transform.AliasToEntityMapResultTransformer;
 import org.hibernate.transform.Transformers;
 import org.springframework.jdbc.object.SqlQuery;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
-
+@AllArgsConstructor
 @Repository
 public class DynamicSqlRepo {
-    @PersistenceContext
-    private EntityManager entityManager;
+
+    private final EntityManager entityManager;
 
     public List<Map<String, Object>> executeQuery(String sql){
-        Session session = entityManager.unwrap(Session.class);
-        Query query = session.createNativeQuery(sql);
-        query.unwrap(NativeQuery.class).setTupleTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
 
-        return query.getResultList();
+        return entityManager.unwrap(Session.class)
+                .createNativeQuery(sql).unwrap(NativeQuery.class)
+                .setTupleTransformer(Transformers.ALIAS_TO_ENTITY_MAP)
+                .getResultList();
 
+    }
+
+
+
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public  Data hashPassword(String planeText) {
+        byte[] salt = HashWithValidation.generateSalt();
+        String hash = HashWithValidation.hashPassword(planeText, salt);
+
+        return new Data(hash, Base64.getEncoder().encodeToString(salt));
+    }
+
+    public  boolean isExpectedPassword(String planeText, String salt, String hash) {
+        return (HashWithValidation.isExpectedPassword(planeText, Base64.getDecoder().decode(salt.getBytes(StandardCharsets.UTF_8)), hash)) ;
+
+
+    }
+
+    record Data(String hash, String salt){
 
     }
 
