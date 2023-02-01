71389f62ef8592d98fdf6b1aa1c447ea9d56044a
Assaduzzaman <a2z.ice@gmail.com>
2023-02-02 04:36:42 +0600

    hasmap transformer

--- cache/l2cache/src/main/java/com/example/l2cache/DynamicSqlRepo.java
+++ cache/l2cache/src/main/java/com/example/l2cache/DynamicSqlRepo.java
@@ -3,18 +3,31 @@ package com.example.l2cache;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.transform.AliasToEntityMapResultTransformer;
+import org.hibernate.transform.Transformers;
+import org.springframework.jdbc.object.SqlQuery;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class DynamicSqlRepo {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<Object[]> executeQuery(String sql){
-        Query query = entityManager.createNativeQuery(sql);
+    public List<Map<String, Object>> executeQuery(String sql){
+        Session session = entityManager.unwrap(Session.class);
+        Query query = session.createNativeQuery(sql);
+        query.unwrap(NativeQuery.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+
         return query.getResultList();
+
+
     }
+
+
 
 }
