5400dd67470e10d9e4bccaac7c0e68ea600163d5
Assaduzzaman <a2z.ice@gmail.com>
2023-05-11 11:11:40 +0000

    added openssl example

---
+++ k8s/sslmanagement/ssl-csr.sh
@@ -0,0 +1,12 @@
+openssl genrsa -out ca.key 2048
+openssl req -new -key ca.key -subj "/CN=KUBERNETES-CA" -out ca.csr
+openssl x509 -req -in ca.csr -signkey ca.key -out ca.crt
+
+# Generating the client certificate
+openssl genrsa -out admin.key 2048
+openssl req -new -key admin.key -subj "/CN=kube-admin" -out admin.csr
+# with group (must be included)
+openssl req -new -key admin.key -subj "/CN=kube-admin/O=system:masters" -out admin.csr
+
+openssl x509 -req -in admin.csr -CA ca.crt -CAkey ca.key -out admin.crt
+
