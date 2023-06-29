167a67b97effd0def905955d4fecf168482c6be3
Assaduzzaman Assad <assad@batworld.com>
2023-06-29 16:46:30 +0600

    cleanup

--- tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/commons/PostgresSupportedBaseTest.java
+++ tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/commons/PostgresSupportedBaseTest.java
@@ -1,16 +1,8 @@
 package queue.pro.cloud.qapi.commons;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-//@DataJpaTest
-//@Testcontainers(disabledWithoutDocker = true)
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 
 public abstract class PostgresSupportedBaseTest {
 
