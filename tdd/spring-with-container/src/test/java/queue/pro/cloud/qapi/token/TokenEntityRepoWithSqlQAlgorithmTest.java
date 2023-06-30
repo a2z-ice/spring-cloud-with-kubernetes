3d4e6e97d790db90101db04d67a519bf75375df4
Assaduzzaman Assad <assad@batworld.com>
2023-06-30 11:32:39 +0600

    added optimistic locking test

--- tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/token/TokenEntityRepoWithSqlQAlgorithmTest.java
+++ tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/token/TokenEntityRepoWithSqlQAlgorithmTest.java
@@ -6,9 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
-import queue.pro.cloud.qapi.PostgresSupportedBaseTest;
-import queue.pro.cloud.qapi.YugabyteDBSupportedBaseTest;
 import queue.pro.cloud.qapi.annotation.DataJpaTestWithContainer;
+import queue.pro.cloud.qapi.commons.PostgresSupportedBaseTest;
 import queue.pro.cloud.qapi.token.repo.TokenFilter;
 import queue.pro.cloud.qapi.token.repo.TokenRepo;
 
