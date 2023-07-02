0dafb983dfce29a897a7ecbca38e13e60e3ba58f
Assaduzzaman Assad <assad@batworld.com>
2023-07-02 16:14:05 +0600

    Write unite test for service with StepVerifier

--- tdd/spring-with-container/src/main/java/queue/pro/cloud/qapi/token/TokenSvc.java
+++ tdd/spring-with-container/src/main/java/queue/pro/cloud/qapi/token/TokenSvc.java
@@ -1,14 +1,32 @@
 package queue.pro.cloud.qapi.token;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import queue.pro.cloud.qapi.token.repo.TokenRepo;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
-@RestController
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
 public class TokenSvc {
-    @GetMapping("/tokens")
-    Flux<Integer> getTokens(){
-        return Flux.just(1,2,3);
+
+    private final TokenRepo tokenRepo;
+
+    public Mono<TokenEntity> getTokenById(String tokenId){
+        return Mono.fromSupplier(() -> tokenRepo.findById(tokenId).orElseThrow())
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+
+
+    public Flux<TokenEntity> getTokenAll(){
+        return Flux.fromStream(() -> this.tokenRepo.findAll().stream())
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
 }
---
+++ tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/token/TokenServiceTest.java
@@ -0,0 +1,103 @@
+package queue.pro.cloud.qapi.token;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import queue.pro.cloud.qapi.token.repo.TokenRepo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class TokenServiceTest {
+    @Mock
+    TokenRepo tokenRepo;
+    @InjectMocks
+    private TokenSvc tokenSvc;
+
+    @Test
+    public void shouldNotBeNull(){
+        assertNotNull(tokenRepo);
+        assertNotNull(tokenSvc);
+    }
+
+    @Test
+    @DisplayName("Test when repository return empty")
+    public void testEmptyToken_shouldThrowNoSuchElementException(){
+        //Given
+        String id = "id";
+        //When
+        when(tokenRepo.findById(Mockito.anyString())).thenReturn(Optional.empty());
+        //Then
+        Mono<TokenEntity> token = tokenSvc.getTokenById(id);
+        //Verify
+        StepVerifier.create(token)
+                .expectError(NoSuchElementException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("When repository return existing token should match given id")
+    public void testExistingToken_shouldMatchWithGivenId(){
+        String id = "id";
+        when(tokenRepo.findById(Mockito.anyString())).thenReturn(Optional.of(TokenEntity.builder().id(id).build()));
+        Mono<TokenEntity> token = tokenSvc.getTokenById(id);
+
+        StepVerifier.create(token)
+                .assertNext(tokenEntity -> {
+                    assertThat(tokenEntity.getId()).isEqualTo(id);
+                }).verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test getTokenAll - Verify Each Element ID and total size is 3")
+    public void testGetTokenAll_VerifyEachElementId() {
+        //given
+        String expectedId = "exampleTokenId";
+        List<TokenEntity> tokenEntities = Arrays.asList(
+                TokenEntity.builder().id(expectedId).build(),
+                TokenEntity.builder().id(expectedId).build(),
+                TokenEntity.builder().id(expectedId).build()
+
+        );
+        //When
+        when(tokenRepo.findAll()).thenReturn(tokenEntities);
+        //Then
+        Flux<TokenEntity> result = tokenSvc.getTokenAll();
+        //Verify
+        StepVerifier.create(result)
+                .expectNextMatches(tokenEntity -> tokenEntity.getId().equals(expectedId))
+                .expectNextMatches(tokenEntity -> tokenEntity.getId().equals(expectedId))
+                .expectNextMatches(tokenEntity -> tokenEntity.getId().equals(expectedId))
+                .verifyComplete();
+        assertThat(tokenEntities).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("Test getTokenAll - Empty List")
+    public void testGetTokenAll_EmptyList() {
+        List<TokenEntity> emptyTokenEntities = Collections.emptyList();
+
+        // Mock the tokenRepo.findAll() method to return an empty list as a stream
+        when(tokenRepo.findAll()).thenReturn(emptyTokenEntities);
+
+        // Invoke the getTokenAll method and verify using StepVerifier
+        Flux<TokenEntity> result = tokenSvc.getTokenAll();
+
+        StepVerifier.create(result)
+                .expectComplete()
+                .verify();
+
+        assertThat(emptyTokenEntities).isEmpty();
+    }
+}
