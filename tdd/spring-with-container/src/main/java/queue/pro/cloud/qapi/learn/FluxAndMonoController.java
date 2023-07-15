5eb46796306456fa59d3c7f5fdaad3c3c4f04790
baassad <assad@batworld.com>
2023-07-16 00:16:23 +0600

    refactor

--- tdd/spring-with-container/src/main/java/queue/pro/cloud/qapi/learn/FluxAndMonoController.java
+++ tdd/spring-with-container/src/main/java/queue/pro/cloud/qapi/learn/FluxAndMonoController.java
@@ -1,12 +1,15 @@
 package queue.pro.cloud.qapi.learn;
 
 
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class FluxAndMonoController {
@@ -18,5 +21,10 @@ public class FluxAndMonoController {
     @GetMapping("/mono")
     Mono<String> mono(@AuthenticationPrincipal Mono<Jwt> jwtPrincipal){
         return jwtPrincipal.map(Jwt::getSubject).log();
+    }
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .log();
     }
 }
--- tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/learn/FluxAndMonoControllerJwtMockTest.java
+++ tdd/spring-with-container/src/test/java/queue/pro/cloud/qapi/learn/FluxAndMonoControllerJwtMockTest.java
@@ -1,40 +1,30 @@
 package queue.pro.cloud.qapi.learn;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockJwt;
-import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockOAuth2Login;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.*;
 
 @WebFluxTest(controllers = FluxAndMonoController.class)
 @AutoConfigureWebTestClient
 public class FluxAndMonoControllerJwtMockTest {
     @Autowired
     WebTestClient webTestClient;
-
-    static SecurityMockServerConfigurers.JwtMutator mockedJwt;
-
-    @BeforeAll
-    static void  setMockedJwt(){
-        mockedJwt = SecurityMockServerConfigurers.mockJwt().jwt(jwt -> {
-            jwt.claims(claims -> {
-                claims.put("scope", "message-read");
-            }).subject("client");
-        });
-    }
-
+    final static JwtMutator mockedJwt = mockJwt().jwt(jwt -> jwt.claims(claims -> {
+        claims.put("scope", "message-read");
+    }).subject("client"));
 
     @Test
     void flux_onlyAuthenticationNoClaimsDefine(){
-
-
         webTestClient
                 .mutateWith(mockOAuth2Login())
                 .get()
@@ -45,10 +35,39 @@ public class FluxAndMonoControllerJwtMockTest {
                 .expectBodyList(Integer.class)
                 .hasSize(3);
     }
+    @Test
+    void flux_secondApproach(){
+        final Flux<Integer> result = webTestClient
+                .mutateWith(mockOAuth2Login())
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(result)
+                .expectNext(1,2,3)
+                .verifyComplete();
+    }
 
     @Test
+    void mono_testUsingConsumeWith(){
+        webTestClient.mutateWith(mockOAuth2Login())
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    final List<Integer> responseBody = listEntityExchangeResult.getResponseBody();
+                    assert (Objects.requireNonNull(responseBody).size() == 3);
+                });
+    }
+    @Test
     void mono_withClaimsAddedToJwtToken(){
-
         final Flux<String> response = webTestClient
                 .mutateWith(mockedJwt)
                 .get()
@@ -63,4 +82,36 @@ public class FluxAndMonoControllerJwtMockTest {
                 .expectNext("client")
                 .verifyComplete();
     }
+    @Test
+    void mono_anotherApproach(){
+        webTestClient.mutateWith(mockedJwt)
+                .get()
+                .uri("/mono")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .consumeWith(stringEntityExchangeResult -> {
+                    final String responseBody = stringEntityExchangeResult.getResponseBody();
+                    assertEquals("client",responseBody);
+                });
+    }
+
+    @Test
+    void stream(){
+        final Flux<Long> result = webTestClient.mutateWith(mockOAuth2Login())
+                .get()
+                .uri("/stream")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(result)
+                .expectNext(0L,1L,2L,3L)
+                .thenCancel()
+                .verify();
+    }
+
 }
